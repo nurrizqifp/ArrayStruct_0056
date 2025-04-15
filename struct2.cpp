dbff2fb263029c98c5b618b567ff2968c08124e0
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-15 16:20:32 +0700

    Membuat input untuk Data Mahasiswa

--- struct2.cpp
+++ struct2.cpp
@@ -6,3 +6,27 @@ struct detailAlamat{
     string desa;
     string kota;
 };
+
+//membuat struct mahasiswa
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    //membuat objek mhs untuk struct mahasiswa pada program utama
+    mahasiswa mhs;
+
+    //Membuat input untuk Data Mahasiswa
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+}
