7efa83c23b65d816dd8451a20c802e4d60cdf9e1
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-15 16:27:14 +0700

    menambahkan ignore untuk mengabaikan cin

---
+++ struct3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//membuat struct untuk detail alamat
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+//membuat struct mahasiswa
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    //membuat objek mhs untuk struct mahasiswa pada program utama
+    mahasiswa mhs;
+
+    //Membuat input untuk Data Mahasiswa
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore(); //menambahkan ignore untuk mengabaikan cin
+    cout << "Masukkan Nama = ";
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    //Display Data Mahasiswa
+    cout << "Data Mahasiswa" << endl;
+    
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+}
