14a3662fd19aa6d96c15c761342a8824e335c3f4
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-15 16:15:37 +0700

    membuat objek mhs untuk struct mahasiswa pada program utama

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,26 @@ struct mahasiswa{
     string nama;
     string alamat;
 };
+
+int main(){
+    //membuat objek mhs untuk struct mahasiswa pada program utama
+    mahasiswa mhs;
+
+    mhs.nim = "2024014001";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+    
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
