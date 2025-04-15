7de09a9da6014807adbb063f97a151d4af301a03
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-15 16:44:42 +0700

    perulangan untuk display

--- struct4.cpp
+++ struct4.cpp
@@ -18,7 +18,7 @@ int main(){
     //menentukan banyak data yang akan dimasukkan ke array
     mahasiswa mhs[2];
 
-    for(int i = 0; i < 2; i++){ //perulangan untuk mengulangi sebanyak index array input
+    for(int i = 0; i < 2; i++){ //perulangan untuk mengulangi sebanyak index array
     cout << "Mahasiswa ke-" << i+1 << endl;
     cout << "Masukkan NIM = ";
     cin >> mhs[i].nim;
@@ -34,4 +34,12 @@ int main(){
     cout << endl;
     //Display Data Mahasiswa
     cout << "Data Mahasiswa" << endl;
+    
+    for (int i = 0; i < 2; i++) { //perulangan untuk display
+    cout << "Data Mahasiswa ke-" << i+1 << endl;
+    cout << "NIM = " << mhs[i].nim << endl;
+    cout << "Nama = " << mhs[i].nama << endl;
+    cout << "Alamat Desa = " << mhs[i].alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs[i].alamat.kota << endl;
+    }
 }
