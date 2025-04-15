58af35355d46dc9782c9f66df512bf91d7a4960f
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-04-15 15:12:28 +0700

    deklarasi array dengan ukuran dan diberi nilai awal

--- array.cpp
+++ array.cpp
@@ -3,3 +3,5 @@ using namespace std;
 
 //deklarasi array tanpa ukuran, tetapi wajib mengisikan data
 int nilai[] = {2,5,9,10,34};
+//deklarasi array dengan ukuran dan diberi nilai awal
+int skor[5] = {8,12,18,5};
